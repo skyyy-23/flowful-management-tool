f5bdf4f773e0c00eab1d4178ddb36fdc844aae5b
Sean Kirby Alipao <alipao.312421@calamba.sti.edu.ph>
2026-03-26 16:18:21 +0800

    Added an assignee and assigned flow in route

--- back-end/app/Http/Controllers/Api/TaskController.php
+++ back-end/app/Http/Controllers/Api/TaskController.php
@@ -5,27 +5,61 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Models\OrganizationMember;
+use App\Models\Project;
 
 class TaskController extends Controller
 {
     public function store(Request $request){
         $request->validate([
             'project_id' => 'required',
-            'title' => 'required'
+            'title' => 'required',
+            'assigned_to' => 'nullable|exists:users,id'
         ]);
-        $task = Task::create([
-            'project_id' => $request->project_id,
-            'parent_id' => $request->parent_id, // optional
-            'title' => $request->title,
-            'description' => $request->description,
-            'status' => 'todo',
-            'assigned_to' => $request->assigned_to
-        ]);
+
+        $project = Project::findOrFail($request->project_id);
+
+        if ($request->assigned_to) {
+            $isMember = OrganizationMember::where('user_id', $request->assigned_to)
+                        ->where('organization_id', $project->organization_id)
+                        ->exists();
+
+            if (!$isMember) {
+                return response()->json([
+                    'message' => 'User is not part of this organization'
+                ], 403);
+            }
+        }
+
+        $task = Task::create($request->all());
+
         return response()->json($task);
     }
     public function update(Request $request, $id){
         $task = Task::findOrFail($id);
-        $task->update($request->only('title', 'description', 'status'));
+        
+        $request->validate([
+            'title' => 'sometimes|required',
+            'description' => 'nullable',
+            'status' => 'sometimes|in:todo,doing,done',
+            'assigned_to' => 'nullable|exists:user,id'
+        ]);
+
+        $project = Project::findOrFail($task->project_id);
+        
+        if($request->assigned_to){
+            $isMember = OrganizationMember::where('user_id', $request->assigned_to)
+                        ->where('organization_id', $project->organization_id)
+                        ->exists();
+
+            if (!$isMember) {
+                return response()->json([
+                    'message' => 'User is not part of this organization'
+                ], 403);
+            }
+        }
+
+        $task->update($request->only(['title', 'description', 'status', 'assigned_to']));
 
         return response()->json($task);
     }
